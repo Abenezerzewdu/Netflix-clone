1d4977b90160870a6d9ed76e2e6a74187116392a
abenezer <abenezerzewdu11@gmail.com>
2025-03-18 22:20:30 +0300

    header and  footer added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import NetflixLogo from "../../assets/Images/netflixLogo.png";
+function Header() {
+  return (
+    <div className="header-outer-container">
+      <div className="header-container">
+        <div className="header-left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix-logo" width="100"></img>
+            </li>
+            <li>Netflix</li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyLists</li>
+            <li>Browse By Language</li>
+          </ul>
+        </div>
+        <div className="header-right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Components/Header/Header.js";
+import Footer from "../Components/Footer/Footer.js";
+function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
