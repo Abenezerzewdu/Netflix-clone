0b2618e665bedc5febec94a5c076cee8dd16d950
abenezer <abenezerzewdu11@gmail.com>
2025-03-21 04:01:58 +0300

    Banner

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Components/Header/Header.js";
 import Footer from "../Components/Footer/Footer.js";
+import Banner from "../Components/Banner/Banner.js";
 function Home() {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   );
